3895a049b7891a92612563a70e3c861f5e1906db
EvgeniaS <150538350+calicat75@users.noreply.github.com>
2026-07-05 21:21:21 +0300

    Update Account.java

--- src/main/java/com/example/praktika_neoflex/entity/Account.java
+++ src/main/java/com/example/praktika_neoflex/entity/Account.java
@@ -1,9 +1,9 @@
 package com.example.praktika_neoflex.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
 import com.example.praktika_neoflex.enums.AccountStatus;
 import com.example.praktika_neoflex.enums.OwnerType;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "accounts")
+@Table(name = "accounts", schema = "accounts")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -23,74 +23,62 @@ public class Account {
     @Id
     private UUID id;
 
-        @Column(name = "account_number",
-                nullable = false,
-                unique = true)
-        private String accountNumber;
+    @Column(name = "account_number", nullable = false, unique = true)
+    private String accountNumber;
 
-        @Column(name = "owner_id",
-                nullable = false)
-        private UUID ownerId;
+    @Column(name = "owner_id", nullable = false)
+    private UUID ownerId;
 
-        @Enumerated(EnumType.STRING)
-        @Column(name = "owner_type",
-                nullable = false)
-        private OwnerType ownerType;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "owner_type", nullable = false)
+    private OwnerType ownerType;
 
-        @ManyToOne(fetch = FetchType.EAGER)
-        @JoinColumn(name = "currency_id")
-        private Currency currency;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "currency_id")
+    private Currency currency;
 
-        @ManyToOne(fetch = FetchType.EAGER)
-        @JoinColumn(name = "account_type_id")
-        private AccountTypeEntity accountType;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "account_type_id")
+    private AccountTypeEntity accountType;
 
-        @Column(nullable = false,
-                precision = 19,
-                scale = 2)
-        @Builder.Default
-        private BigDecimal balance = BigDecimal.ZERO;
+    @Column(nullable = false, precision = 19, scale = 2)
+    @Builder.Default
+    private BigDecimal balance = BigDecimal.ZERO;
 
-        @Enumerated(EnumType.STRING)
-        @Column(name = "account_status")
-        @Builder.Default
-        private AccountStatus accountStatus = AccountStatus.ACTIVE;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "account_status")
+    @Builder.Default
+    private AccountStatus accountStatus = AccountStatus.ACTIVE;
 
-        @Column(name = "created_at")
-        private LocalDateTime createdAt;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 
-        @Column(name = "updated_at")
-        private LocalDateTime updatedAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
-        @Column(name = "created_by")
-        private String createdBy;
+    @Column(name = "created_by")
+    private String createdBy;
 
-        @Column(name = "updated_by")
-        private String updatedBy;
+    @Column(name = "updated_by")
+    private String updatedBy;
 
-        @Version
-        private Long version;
+    @Version
+    private Long version;
 
-        @OneToMany(mappedBy = "account",
-                cascade = CascadeType.ALL,
-                orphanRemoval = true)
+    @OneToMany(mappedBy = "account",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
     @Builder.Default
     private List<AccountStatusHistory> history = new ArrayList<>();
 
     @PrePersist
     public void prePersist() {
-
         createdAt = LocalDateTime.now();
-
         updatedAt = LocalDateTime.now();
-
     }
 
     @PreUpdate
     public void preUpdate() {
-
         updatedAt = LocalDateTime.now();
-
     }
-
 }
